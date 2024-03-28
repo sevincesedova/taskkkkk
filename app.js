6d344c213f91eda98d3dc21873c8db58dcd627aa
sevincesedova <161349938+sevincesedova@users.noreply.github.com>
2024-03-28 18:07:31 +0400

    sinif_tapsiriq

---
+++ app.js
@@ -0,0 +1,81 @@
+// task1
+// let numbers=[1,3,5,7,9,2,8,6,4,0]
+// console.log(numbers)
+// const numbers_sort=numbers.sort((a,b)=>b-a)
+// console.log(numbers[0])
+
+
+
+// task2
+//  let numbers=[1,5,7,9,2,8,6,4,13,41,44]
+//     function sade(num){
+//         for(i=2; i<=num/2; i++){
+//             if(num%i===0){
+//                 return false
+//             }
+//         }
+//           return true
+//     }
+//     const numbers_filter=numbers.filter(sade)
+//     console.log(numbers_filter)
+
+
+    // task3
+// let data=["salam","dunyali","javascript","proqramlasdirma","kod","frontend","backend"]
+// console.log(data)
+// const data_filter=data.filter((data) => data.length ===7)
+// console.log(data_filter)
+
+
+// task4
+// let kilo=+prompt("kilonuzu daxil edin:")
+// let boy=+prompt("boyunuzu m ile daxil edin:")
+// let kutle_indeksi=kilo/boy**2
+
+// if(kutle_indeksi<18){
+//     alert("ideal çekiden asagi")
+// }
+// else if(kutle_indeksi>=18 && kutle_indeksi<25){
+//    alert("ideal çeki")
+// }
+// else if(kutle_indeksi>=25 && kutle_indeksi<30){
+//     alert("ideal çekiden yuxari")
+// }
+// else if(kutle_indeksi>=30 && kutle_indeksi<40){
+//     alert("artiq ceki")
+// }
+// else {
+//     alert("piylenme")
+// }
+
+// task5
+// let bal=prompt("balinizi daxil edin")
+// if(bal<=100 && bal>90){
+//     alert("ela")
+// }
+// else if(bal<=90 && bal>75){
+//     alert("yaxsi")
+// }
+// else if(bal<=75 && bal>55){
+//     alert("orta")
+// }
+// else if(bal<=55 && bal>35){
+//     alert("pis")
+// }
+// else if(bal<=35){
+//     alert("kafi")
+// }
+// else {
+//     alert("balinizi duzgun daxil edin")
+// }
+
+// task6
+// let a= + prompt("ucbucagin birinci terefini daxil edin:")
+// let b= +prompt("ucbucagin birinci terefini daxil edin:")
+// function hipotenuz(a,b) {
+//     c=a**2+b**2;
+//     k=Math.sqrt(c);
+//   return k; 
+// }
+
+// alert(hipotenuz(a,b))
